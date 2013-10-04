4f2733a3dfc3bafb72246e7043f9f87f0f061263
Seyyed Morteza Moosavi <se.mo.moosavi@gmail.com>
2013-10-04 23:58:05 +0330

    add pg-test1/www/phonegap.js to gitignore.

--- pg-test1/www/js/index.js
+++ pg-test1/www/js/index.js
@@ -33,6 +33,7 @@ var app = {
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicity call 'app.receivedEvent(...);'
     onDeviceReady: function() {
+
         app.receivedEvent('deviceready');
     },
     // Update DOM on a Received Event
@@ -45,5 +46,10 @@ var app = {
         receivedElement.setAttribute('style', 'display:block;');
 
         console.log('Received Event: ' + id);
+
+        $("#btn").on('touchstart',function(){
+            console.log('test');
+            $('#console').append('test\n')
+        });
     }
 };
