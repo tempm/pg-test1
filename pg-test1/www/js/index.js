923ec0c9e8d3b1bbc7a1c128d408c08c8c741c50
Seyyed Morteza Moosavi <se.mo.moosavi@gmail.com>
2013-10-05 06:42:17 +0330

    removeing old css and scripts and adding bootstrap example

--- pg-test1/www/js/index.js
+++ pg-test1/www/js/index.js
@@ -18,42 +18,26 @@
  */
 var app = {
     // Application Constructor
-    initialize: function() {
+    initialize:function () {
         this.bindEvents();
     },
     // Bind Event Listeners
     //
     // Bind any events that are required on startup. Common events are:
     // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
+    bindEvents:function () {
         document.addEventListener('deviceready', this.onDeviceReady, false);
     },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicity call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
-        console.log($("#btn").length);
-        $("#btn").on('touchstart',function(){
+
+    onDeviceReady:function () {
+
+        $("#btn").on('touchstart',function () {
             console.log('touchstart');
             $('#console').append('touchstart\n')
-        });
-        $("#btn").on('click',function(){
-            console.log('click');
-            $('#console').append('click\n')
-        });
-
-        app.receivedEvent('deviceready');
-    },
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
-
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
-
-        console.log('Received Event: ' + id);
+        }).on('click', function () {
+                console.log('click');
+                $('#console').append('click\n')
+            });
     }
+
 };
